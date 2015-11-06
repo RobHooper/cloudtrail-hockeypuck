97cc8e9f6bdd0e7551542e7658882db774e208fa
RobHooper <rob@dogsbodytechnology.com>
2015-11-06 16:40:32 +0000

    example conf

---
+++ example-ctconfig.py
@@ -0,0 +1,14 @@
+## Usage
+# Rename this file to ctconfig.py and set the variables
+#
+## Needed variables
+# AWS Details
+AWSPROFILE=''
+AWSREGION=''
+AWSCTQUEUE=''
+# MySQL Details
+hostname=""
+username=""
+password=""
+database=""
+tablename=""
